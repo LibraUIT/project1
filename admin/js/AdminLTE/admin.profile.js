56e710ed2e998ead4437d4661f7d197f44d2d605
Libra Nguyen <quanmn.libra@gmail.com>
2015-07-17 00:02:01 +0700

    delete source

--- admin/js/AdminLTE/admin.profile.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-configControllers.controller('ProfileController', ['$scope', '$rootScope', '$route','$routeParams', '$location','$http', '$state', 'CategoryService',
-  function($scope, $rootScope, $route ,$routeParams, $location, $http, $state, CategoryService) {
-    document.title = 'AdminLTE | Profile';
-    
-}]);
--- public/admin/admin.common.js
+++
@@ -1,8 +0,0 @@
-$.ajax({
-		method: "POST",
-		url: [base_url, 'admin', 'lang'].join('/'),
-		data: { }
-		              })
-		.done(function( result ) {
-		   lang = result;
-});
